94a11c3570bdbdbfa4a28c0aa9adcaec9fe30636
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2024-01-22 11:20:15 +0100

    Change footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
+          Created by <a href="https://nicolevanderhoeven.com">Nicole van der Hoeven</a>, © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
